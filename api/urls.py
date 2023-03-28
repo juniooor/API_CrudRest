e5258820f687c84a632148e3e7fbdedaf55f4d09
Juniooor <juniormodern@hotmail.com>
2023-03-27 22:27:39 -0300

    crud api completed

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,15 @@
 
+from core.views import CustomerViewSet
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'Customers', CustomerViewSet)
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
-     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from .models import Customer
+from .serializers import CustomerSerializer
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+ 
+   
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = [permissions.IsAuthenticated]
